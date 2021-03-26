a76854315625660ef563c6e687a073302827f088
Andrey Nevolin <anevolin@aligntech.com>
2021-03-26 14:22:23 +0300

    Fields added to "Options" structure

--- getopt.go
+++ getopt.go
@@ -5,6 +5,14 @@ type Option struct {
 	LongName string
 	// Short (or single-character) name of the option
 	ShortName rune
+	// Specifies how results are returned for an option. If "Flag" is nil, then
+	// Parser.getopt() sets one of its return values to "Val". Otherwise, Parser.getopt()
+	// sets the return value to 0, and memory location referenced by "Flag" is set to
+	// "Val" if the option is found, but left unchanged if the option is not found
+	Flag *int
+	// The value to be returned by Parser.getopt(), or to be loaded into the memory
+	// location pointed to by "Flag"
+	Val int
 }
 
 type Parser struct {
